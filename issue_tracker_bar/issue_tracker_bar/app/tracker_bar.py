f1e3183a63ed85872a52b338ce14e809b2534fcd
lajidey <olajamesjide@gmail.com>
2022-05-02 02:54:24 +0000

    Sub version: dynamic status flexible style, good user readability display indicators

--- issue_tracker_bar/issue_tracker_bar/app/tracker_bar.py
+++ issue_tracker_bar/issue_tracker_bar/app/tracker_bar.py
@@ -10,9 +10,21 @@ from frappe.model.document import Document
 def get_all_task(issue=None):
     """ all task linked to single issue"""
     issue = issue
+    task_list = []
     task_list = frappe.db.sql(""" 
-    SELECT name,status,subject FROM `tabTask` WHERE docstatus <> 2 AND issue = '{0}'
-    """.format(issue), as_dict=1 )
+    SELECT name,status,subject FROM `tabTask` WHERE docstatus <> 2 AND issue = '%s'
+    """%issue, as_dict=1 )
+
+    """get all  available status of task"""
+    tmeta = frappe.get_meta('Task')
+    tstatus = tmeta.get_field('status').options.split("\n")
+
+    """ get all color tagging for task"""
+    color_tast_tagr = frappe.db.get_single_value('Issue Task Bar', 'task_status_style').split("\n")
+    
+    for tl in task_list:
+        indexitm = tstatus.index(tl.status)
+        tl['tcolor'] = color_tast_tagr[indexitm]
 
     #print(f"\n\n\n\n\n {task_list} \n\n\n")
     return task_list
--- issue_tracker_bar/issue_tracker_bar/app/utils.js
+++ issue_tracker_bar/issue_tracker_bar/app/utils.js
@@ -3,50 +3,33 @@ frappe.ui.form.on("Issue", {
 		if (frm.doc.docstatus === 0 && !frm.is_new()) {
 			frm.trigger("show_progress_for_Issues");
 		}
+		
 	},
 
     show_progress_for_Issues: function(frm) {
-		/**Standard Task Status 
-		 * Open
-		 * Working
-		 * Pending Review
-		 * Overdue
-		 * Template
-		 * Completed
-		 * Cancelled
-		*/
-		//css_list: 1:primary,2:warning,3:danger,4:success| unknow: info,secondary,dark & light
-		
 		let bars = [];
 		let message = '';
 		let title = '';
-		let barlenght = 0;
+		let nwstyle = ";background-color:";
+		var status_list=[] ;
 
-		let progress_class = [
-			{tstatus:"Open",tstep:1, tclass:"progress-bar-danger"},
-			{tstatus:"Working",tstep:1,tclass:"progress-bar-warning"},
-			{tstatus:"Pending",tstep:2,tclass:"progress-bar-primary"} ,
-			{tstatus:"Review",tstep:3,tclass:"progress-bar-info"},
-			{tstatus:"Completed",tstep:4,tclass:"progress-bar-success"}
-		];
-
+		/*api call: .replace(/;+$/,"")*/
 		frappe.call({
 			method:"issue_tracker_bar.issue_tracker_bar.app.tracker_bar.get_all_task",
 			args:{issue:frm.doc.name},
 			callback: function (r) {
 				if (r.message.length > 0) {
+					//console.log(r.message);
 					let m_tasks = r.message;
 					
 					m_tasks.forEach(t => {
-						title = __("[<i>{0}</i>]: {1}, <span style='padding-right:4px'></span>", [t.subject ,t.status.bold()]);
-						let tsc = progress_class.find(function(p, i){
-							if(p.tstatus == t.status)
-							return true;
-						});
+						title = __("<i class='indicator-pill whitespace-nowrap {2}'>{0}</i>: \
+						<span style='padding-right:10px'>{1} </span>", [t.subject ,t.status.bold(),t.tcolor]);
+						
 						bars.push({
 							'title': title,
-							'width': 1 / m_tasks.length * 100  + '%',
-							'progress_class': tsc.tclass
+							'width': 1 / m_tasks.length * 100  + '%'+nwstyle+t.tcolor+';',
+							'progress_class': ''
 						});
 						message += title;						
 						
@@ -56,7 +39,16 @@ frappe.ui.form.on("Issue", {
 				}
 				
 			}
-		});			
+		});
 		
 	},
 });
+
+/***
+ <a class="badge-wrapper bullet" href="/c/erpnext/support">
+	<span class="badge-category-bg" style="background-color: #0088CC;"></span>
+	<span data-drop-close="true" class="badge-category clear-badge" title="">
+	<span class="category-name">Support</span>
+	</span>
+</a>
+ */
